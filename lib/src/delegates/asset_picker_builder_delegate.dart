9747068b088ed4a35d11f2234f8d22a46821d919
lly <576061110@qq.com>
2026-09-20 15:21:32 +0800

    add skip func

--- lib/src/delegates/asset_picker_builder_delegate.dart
+++ lib/src/delegates/asset_picker_builder_delegate.dart
@@ -1868,7 +1868,7 @@ class DefaultAssetPickerBuilderDelegate<T extends DefaultAssetPickerProvider>
             borderRadius: BorderRadius.circular(3),
           ),
           onPressed: () {
-            Navigator.maybeOf(context)?.maybePop(shouldAllowConfirm?p.selectedAssets:[]);
+            Navigator.maybeOf(context)?.maybePop(shouldAllowConfirm?p.selectedAssets:List<AssetEntity>.empty());
           },
           materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
           child: ScaleText(
